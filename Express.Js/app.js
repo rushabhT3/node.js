d081b7cc3a3f1579e00145e5c5f7aaabf6fb5551
rushabhT3 <41021094+rushabhT3@users.noreply.github.com>
2023-04-16 12:07:53 +0530

    form submission, body-parse,  req.body, res.redirect

--- Express.Js/app.js
+++ Express.Js/app.js
@@ -1,17 +1,31 @@
 // can bring this code from the Express Website
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
 const port = 80;
 
+app.use(bodyParser.urlencoded());
+
 // defining the middleware using the .use() and the next() function is for passing the control to the next middleware
-app.use((req, res, next) => {
-    console.log('this the 1st middleware');
-    next();
+app.use('/add-product', (req, res, next) => {
+    res.send(`<form action="/product" method="post">
+                <label for="name">Product</label>
+                <input type="text" id="name" name="name">
+                <label for="size">Size</label>
+                <input type="number" id="size" name="size">
+                <button type="submit">Submit!</button>
+            </form>`);
 });
 
-app.use((req, res, next) => {
-    console.log('this the 2nd middleware');
-    res.send('This is the Express way to send the response');
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('this is the main page');
     // otherwise we would have to write the .write or .read etc.
 });
 
