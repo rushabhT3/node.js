d2886384a0ff4ee5d8ec58ea5db85ca8470c7892
rushabhT3 <41021094+rushabhT3@users.noreply.github.com>
2023-04-16 12:50:06 +0530

    2.0 version: multiple parse, routing, input forms

--- Express.Js/app.js
+++ Express.Js/app.js
@@ -1,12 +1,11 @@
 // can bring this code from the Express Website
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = 80;
 
-app.use(bodyParser.urlencoded());
+app.use(express.urlencoded({extended: true}));
 
 // defining the middleware using the .use() and the next() function is for passing the control to the next middleware
 app.use('/add-product', (req, res, next) => {
@@ -19,11 +18,10 @@ app.use('/add-product', (req, res, next) => {
             </form>`);
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
-
 app.use('/', (req, res, next) => {
     res.send('this is the main page');
     // otherwise we would have to write the .write or .read etc.
