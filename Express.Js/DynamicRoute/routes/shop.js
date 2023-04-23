92bfe44af776d70673ea328c3ff0f736797143c3
rushabhT3 <41021094+rushabhT3@users.noreply.github.com>
2023-04-23 11:49:31 +0530

    Dynamic Route 1 Udemy Code

---
+++ Express.Js/DynamicRoute/routes/shop.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+// It means :productId can be anything, place /products/delete etc. b4 this line
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
