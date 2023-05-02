2333a775379fab25da301f4e97ad15f5fb031607
rushabhT3 <41021094+rushabhT3@users.noreply.github.com>
2023-05-03 05:06:48 +0530

    Cart is working, deleting from cart is working

--- Express.Js/mysqldb/app.js
+++ Express.Js/mysqldb/app.js
@@ -1,10 +1,14 @@
 const path = require('path');
-const express = require('express');
+const express = require('express')
+
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+// in saare models/tables ko bana k yaha pr import kr rhe hain hum
 const Product = require('./models/product')
 const User = require('./models/user')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 
 const app = express();
 
@@ -37,25 +41,32 @@ app.use(errorController.get404);
 // Product and the User have the association/relationship: we are working with those associations
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 // creates tables in the database based on the models you have defined in your code
 // .sync() function creates database tables based on the models
 // {force: true}: already existing tables with same name, will be deleted and recreated.
 // This code uses Sequelize to synchronize the defined models with the database. 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1);
   })
   .then(user => {
     if (!user) {
       // creates a new instance of the User model and adds it to the database. 
-      return User.create({ name: 'Max', email: 'test@test.com' });
+      return User.create({ name: 'XXX', email: 'test@test.com' });
     }
     return user;
   })
   .then(user => {
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(80);
   })
   .catch(err => {
