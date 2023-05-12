6280f4c9bd5b19ab1de05164bd28d47f655cf279
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-05-12 08:49:51 +0530

    bcrypt is added: signup and login controller

--- Express.Js/AWSapp/controllers/controller.js
+++ Express.Js/AWSapp/controllers/controller.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 const { sequelize, User } = require("../models/database");
 
 exports.signUp = async (req, res) => {
@@ -10,10 +12,12 @@ exports.signUp = async (req, res) => {
       return res
         .status(400)
         .json({ error: "bad parameters: something is missing" });
-    } else {
-      await User.create({ name, email, password });
-      res.status(201).json({ message: "Successfully created new user:)" });
     }
+    // ? generating the hash value using the bycrypt
+    bcrypt.hash(password, 10, async (error, hash) => {
+      await User.create({ name, email, password: hash });
+      res.status(201).json({ message: "Successfully created new user:)" });
+    });
   } catch (error) {
     res.status(500).json(error);
   }
@@ -24,15 +28,20 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
     const foundUser = await User.findOne({ where: { email } });
     if (foundUser) {
-      if (foundUser.password == password) {
-        res.status(200).json({ message: "Successfully Logged In" });
-      } else {
-        res.status(401).json({ message: "User not authorized" });
-      }
+      // ? compare with the hash with the non hash value and callback me error k baad result hain and result != res
+      bcrypt.compare(password, foundUser.password, (error, result) => {
+        console.log(result, password, foundUser.password);
+        if (result) {
+          console.log(foundUser);
+          res.status(200).json({ message: "Successfully Logged In" });
+        } else {
+          res.json({ message: "Password is wrong" });
+        }
+      });
     } else {
       // Redirect the user to the signup page
       // res.redirect("../public/signUp/signUp.html");
-      res.status(404).json({ message: "User not found" });
+      res.json({ message: "User not found in table" });
     }
   } catch (error) {
     console.log("login error");
