3b25c8af5df03996c4f4a758d41d5b06e26b0965
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-05-29 03:03:40 +0530

    Add files via upload

--- TypeScript/Basic App/routes/todos.js
+++ TypeScript/Basic App/routes/todos.js
@@ -7,23 +7,28 @@ router.get("/", (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post("/todo", (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({ message: "Updated todo", todos: todos });
 });
 router.delete("/todo/:todoId", (req, res, next) => {
-    todos = todos.filter((element) => element.id !== req.params.todoId);
+    const params = req.params;
+    // ? iterate through array take the condition and store the value that statisfy the condition into stored array
+    todos = todos.filter((element) => element.id !== params.todoId);
     res.status(200).json({ message: "Deleted todo", todos: todos });
 });
 router.put("/todo/:todoId", (req, res, next) => {
+    const body = req.body;
+    const params = req.params;
     // ? find the index
-    const index = todos.findIndex((element) => element.id === req.params.todoId);
-    //   ? array[index] = object: {id: array[index].id, text: req.body.text}
+    const index = todos.findIndex((element) => element.id === params.todoId);
+    // ? array[index] = object: {id: array[index].id, text: req.body.text}
     if (index >= 0) {
-        todos[index] = { id: todos[index].id, text: req.body.text };
+        todos[index] = { id: todos[index].id, text: body.text };
         return res.status(200).json({ message: "Updated todo", todos: todos });
     }
 });
