cdbf77309224c2300fae0a1d69a5b8ffa7e0da78
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-05-28 19:31:42 +0530

    tsc --init: config file, typeof (JS), Union type

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,15 +1,27 @@
 // as HTMLInputElement will take care of the .value error by telling that it will have the .value 
 const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
+// ? strict mode tsc init: using the "!" is telling it will be a not null value, solves error of buttonElement line 9
 const buttonElement = document.querySelector('button')!;
 
-const add = (num1:number, num2:number) => num1 + num2;
+const add = (num1:number | string, num2:number | string) =>{
+    if (typeof num1 === 'number' && typeof num2 === 'number'){
+        return num1 + num2;
+    } else if (typeof num1 === 'string' && typeof num2 === 'string'){
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
+};
 
 buttonElement.addEventListener('click', () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
+    const stringResult = add(num1, num2);
     console.log(result);
+    console.log(stringResult);
+    // console.log(add(true, false));
+
 })
 
 console.log(add(1, 6));
