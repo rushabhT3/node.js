09d54cafb1f09ca511492fda1b3f40df0e31e787
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-05-28 20:39:24 +0530

    Add files via upload

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -1,24 +1,31 @@
-"use strict";
-// Object.defineProperty(exports, "__esModule", { value: true });
-// as HTMLInputElement will take care of the .value error by telling that it will have the .value
-var num1Element = document.getElementById("num1");
-var num2Element = document.getElementById("num2");
+// as HTMLInputElement will take care of the .value error by telling that it will have the .value 
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
 // ? strict mode tsc init: using the "!" is telling it will be a not null value, solves error of buttonElement line 9
-var buttonElement = document.querySelector("button");
+var buttonElement = document.querySelector('button');
+var numResults = [];
+var textResults = [];
 var add = function (num1, num2) {
-  if (typeof num1 === "number" && typeof num2 === "number") {
-    return num1 + num2;
-  } else if (typeof num1 === "string" && typeof num2 === "string") {
-    return num1 + " " + num2;
-  }
-  return +num1 + +num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 };
-buttonElement.addEventListener("click", function () {
-  var num1 = num1Element.value;
-  var num2 = num2Element.value;
-  var result = add(+num1, +num2);
-  var stringResult = add(num1, num2);
-  console.log(result);
-  console.log(stringResult);
-  // console.log(add(true, false));
+var printResult = function (resultObj) {
+    console.log(resultObj.val);
+};
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    numResults.push(result);
+    var stringResult = add(num1, num2);
+    textResults.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
 });
+// https://stackoverflow.com/questions/76195252/typescript-compiler-giving-error-redeclare-block-scoped-variable-but-it-is-not-r
+// export {}
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -4,7 +4,19 @@ const num2Element = document.getElementById('num2') as HTMLInputElement;
 // ? strict mode tsc init: using the "!" is telling it will be a not null value, solves error of buttonElement line 9
 const buttonElement = document.querySelector('button')!;
 
-const add = (num1:number | string, num2:number | string) =>{
+const numResults: number[] = [];
+const textResults: string[] = [];
+
+// TS feature: to combine the types 
+type NumOrString = number | string;
+type Result = {val: number, timestamp: Date}
+
+interface ResultObj {
+    val: number;
+    timestamp: Date;
+}
+
+const add = (num1:NumOrString, num2:NumOrString) =>{
     if (typeof num1 === 'number' && typeof num2 === 'number'){
         return num1 + num2;
     } else if (typeof num1 === 'string' && typeof num2 === 'string'){
@@ -13,18 +25,24 @@ const add = (num1:number | string, num2:number | string) =>{
     return +num1 + +num2;
 };
 
+const printResult = (resultObj: ResultObj) => {
+    console.log(resultObj.val);
+}
+
 buttonElement.addEventListener('click', () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
+
     const result = add(+num1, +num2);
+    numResults.push(result as number);
+
     const stringResult = add(num1, num2);
-    console.log(result);
-    console.log(stringResult);
-    // console.log(add(true, false));
+    textResults.push(stringResult as string);
 
+    printResult({val: result as number, timestamp: new Date()});
+
+    console.log(numResults, textResults);
 })
-
-console.log(add(1, 6));
 
 // https://stackoverflow.com/questions/76195252/typescript-compiler-giving-error-redeclare-block-scoped-variable-but-it-is-not-r
 // export {}
